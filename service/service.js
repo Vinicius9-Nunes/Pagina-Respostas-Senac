44f6b9b86fe36dbfea2c47b43035b544fe862578
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-07-01 23:26:02 -0300

    ajustes

--- service/service.js
+++ service/service.js
@@ -1,23 +1,23 @@
 function GetArrayTexts(){
     return [
         {
-            id: "Quero Ser Gold", 
-            name: "Quero Ser Gold", 
-            texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac.
+            Id: "Quero Ser Gold", 
+            Titulo: "Quero Ser Gold", 
+            Texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac.
 Recebemos teu interesse através de nossa plataforma Quero Ser Gold. 
 Como posso te ajudar? 😊`
         },
         {
-            id: "Post de cadastro",
-            name: "Post de cadastro",
-            texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac. 
+            Id: "Post de cadastro",
+            Titulo: "Post de cadastro",
+            Texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac. 
 Recebemos teu interesse através de nossas redes sociais.
 Gostaria de saber como posso te ajudar? 😊`
         },
         {
-            id: "Explicação da Bolsa Própria",
-            name: "Explicação da Bolsa Própria",
-            texto: `A bolsa própria tem um desconto escalonado ou seja:
+            Id: "Explicação da Bolsa Própria",
+            Titulo: "Explicação da Bolsa Própria",
+            Texto: `A bolsa própria tem um desconto escalonado ou seja:
     1° semestre 80% de desconto;
     2° semestre 50% de desconto;
     3° semestre 40% de desconto;
@@ -26,57 +26,57 @@ Gostaria de saber como posso te ajudar? 😊`
 Cursos que passam de 5° semestre ganham 20% nos demais semestres restantes.`
         },
         {
-            id: "Desconto reingresso",
-            name: "Desconto reingresso",
-            texto: `Temos descontos de 50% de desconto no primeiro semestre para aqueles alunos que retornam a estudar conosco.`
+            Id: "Desconto reingresso",
+            Titulo: "Desconto reingresso",
+            Texto: `Temos descontos de 50% de desconto no primeiro semestre para aqueles alunos que retornam a estudar conosco.`
         },
         {
-            id: "Créditos semana",
-            name: "Créditos semana",
-            texto: `Fazendo 20 créditos o aluno estuda 5 dias na semana;
+            Id: "Créditos semana",
+            Titulo: "Créditos semana",
+            Texto: `Fazendo 20 créditos o aluno estuda 5 dias na semana;
 Fazendo 16 créditos o aluno estuda 4 dias na semana;
 Fazendo 12 créditos o aluno estuda 3 dias na semana.
 
 Lembrando que se você fizer menos que 20 créditos o tempo de curso pode passar do estipulado.`
         },
         {
-            id: "Método de ensino",
-            name: "Método de ensino",
-            texto: `Nosso método de ensino é do formato híbrido.
+            Id: "Método de ensino",
+            Titulo: "Método de ensino",
+            Texto: `Nosso método de ensino é do formato híbrIdo.
 Aulas teóricas: Online (ao vivo);
 Aulas práticas Presencias ( na faculdade).`
         },
         {
-            id: "Documentos para matrícula",
-            name: "Documentos para matrícula",
-            texto: `Documentos para matrícula:
+            Id: "Documentos para matrícula",
+            Titulo: "Documentos para matrícula",
+            Texto: `Documentos para matrícula:
 
 Certificado do ensino médio;
-Certidão de nascimento;
+CertIdão de nascimento;
 Documento com foto;
 Preenchimento da ficha cadastral (digitado);
 Preenchimento dos pdfs ( próprio punho).`
         },
 
         {
-            id: "Aproveitamento reingresso",
-            name: "Aproveitamento reingresso",
-            texto: `Para realização do teu reingresso preciso solicitar uma análise.
+            Id: "Aproveitamento reingresso",
+            Titulo: "Aproveitamento reingresso",
+            Texto: `Para realização do teu reingresso preciso solicitar uma análise.
 Essa análise será enviada para o coordenação pedagógica, e em torno de 3 dias úteis saíra o teu aproveitamento de disciplinas e a orientação de matrícula.
 
 Tens interesse em qual turno?`
 },
 
 {
-    id: "Forma de ingresso",
-    name: "Forma de ingresso",
-    texto: `A forma de ingresso é através do nosso vestibular ou com a nota do Enem do ano passo ou ano atual.
-O vestibular é uma prova de redação com temas da atualidade, podes acessar através do link que eu encaminhar e fazer de casa.`
+    Id: "Forma de ingresso",
+    Titulo: "Forma de ingresso",
+    Texto: `A forma de ingresso é através do nosso vestibular ou com a nota do Enem do ano passo ou ano atual.
+O vestibular é uma prova de redação com temas da atualIdade, podes acessar através do link que eu encaminhar e fazer de casa.`
 },
 {
-    id: "Cursos na área de tecnologia",
-    name: "Cursos na área de tecnologia",
-    texto: `Na área de tecnologia temos:
+    Id: "Cursos na área de tecnologia",
+    Titulo: "Cursos na área de tecnologia",
+    Texto: `Na área de tecnologia temos:
 
 Tecnólogo em Defesa Cibernética;
 Tecnólogo em Redes de computadores;
@@ -85,31 +85,31 @@ Tecnólogo em Análise de Desenvolvimento de Sistemas;
 Tecnólogo em ciência de dados e inteligência analítica.`
 },
 {
-    id: "Horários",
-    name: "Horários",
-    texto: `Temos dois horários:
+    Id: "Horários",
+    Titulo: "Horários",
+    Texto: `Temos dois horários:
     
 Manhã das 9h ás 12h
 Noite das 19:30 ás 22h`
 },
 {
-    id: "Matrícula antecipada",
-    name: "Matrícula antecipada",
-    texto: `Como as aulas irão iniciar em agosto já estamos com matrículas abertas este mês, o aluno pode fazer em mais vezes.
+    Id: "Matrícula antecipada",
+    Titulo: "Matrícula antecipada",
+    Texto: `Como as aulas irão iniciar em agosto já estamos com matrículas abertas este mês, o aluno pode fazer em mais vezes.
 É chamado matrícula antecipada.
 O aluno vai poder fazer em mais parcelas o primeiro semestre pois as aulas ainda não iniciaram, se ele deixar para fazer lá no início de agosto o parcelamento vai ser menor por que se for maior pode somar com o próximo semestre (Senac não permite pois é prejudicial ao aluno).
 Então se tu fizer tua matrícula este mês pode fazer em 1+8.`
 },
 {
-    id: "Interesse no curso",
-    name: "Interesse no curso",
-    texto: `Olá , tudo bem? Verificamos que somente visualizou e não respondeu.
+    Id: "Interesse no curso",
+    Titulo: "Interesse no curso",
+    Texto: `Olá , tudo bem? Verificamos que somente visualizou e não respondeu.
 Ainda possuí interesse em nossos cursos ou posso encerrar seu contato?`
 },
 {
-    id: "Cursos na área de Gestão",
-    name: "Cursos na área de Gestão",
-    texto: `Graduações na área de Gestão: 
+    Id: "Cursos na área de Gestão",
+    Titulo: "Cursos na área de Gestão",
+    Texto: `Graduações na área de Gestão: 
 
 Bacharelado em Administração;
 Bacharelado em Ciências Contábeis;
@@ -122,29 +122,29 @@ Tecnólogo em Processos Gerenciais;
 Tecnólogo em Gestão de Recursos Humanos.`
 },
 {
-    id: "Desconto 60 por cento Whats",
-    name: "Desconto 60% Whats",
-    texto: `Boa tarde!\n
+    Id: "Desconto 60 por cento Whats",
+    Titulo: "Desconto 60% Whats",
+    Texto: `Boa tarde!\n
 Tudo bem? 😃\n
 Me chamo Natalí e sou consultora de vendas da Faculdade Senac.\n
 Você foi contemplada com 60% de desconto em toda sua graduação de Administração, através da parceria que temos com sua empresa!!🤩🤩
 Segue abaixo o portfólio do Tecnólogo em Gestão Financeira.\n
-Qualquer dúvida estarei a sua disposição.`
+Qualquer dúvIda estarei a sua disposição.`
 },
 {
-    id: "Desconto 60 por cento Email",
-    name: "Desconto 60% Email",
-    texto: `Boa tarde!\n
+    Id: "Desconto 60 por cento Email",
+    Titulo: "Desconto 60% Email",
+    Texto: `Boa tarde!\n
 Tudo bem? 😃\n    
 Você foi contemplada com 60% de desconto em toda sua graduação, através da parceria que temos com sua empresa!!🤩🤩
 Segue abaixo o portifólio do Bacharelado em Administração.\n    
-Qualquer dúvida, estou à disposição através do whatsApp 51 991341840\n    
+Qualquer dúvIda, estou à disposição através do whatsApp 51 991341840\n    
 Atenciosamente,`
 },
 {
-    id: "Desconto 60 por cento sugestão de curso",
-    name: "60% sugestão de curso",
-    texto: `Boa tarde!\n
+    Id: "Desconto 60 por cento sugestão de curso",
+    Titulo: "60% sugestão de curso",
+    Texto: `Boa tarde!\n
 Tudo bem? 😃\n    
 Você foi contemplada com 60% de desconto em toda sua graduação através da parceria que temos com sua empresa!!🤩🤩
 Segue abaixo as sugestões de cursos que ofertamos.\n\n    
@@ -165,29 +165,29 @@ Tecnólogo em Sistemas para Internet;
 Tecnólogo em Análise de Desenvolvimento de Sistemas;
 Tecnólogo em ciência de dados e inteligência analítica
 Tecnólogo em Produção Multimídia.\n    
-Qualquer dúvida estarei a sua disposição.
+Qualquer dúvIda estarei a sua disposição.
 Contato: 991341840\n    
 Atenciosamente,`
 },
 {
-    id: "Aproveitamento tecnologia",
-    name: "Aproveitamento tecnologia",
-    texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac.\n
-Verificamos que fizeste uma graduação na área de tecnologia conosco e estamos com uma ótima oportunidade!! 🤩🤩\n    
+    Id: "Aproveitamento tecnologia",
+    Titulo: "Aproveitamento tecnologia",
+    Texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac.\n
+Verificamos que fizeste uma graduação na área de tecnologia conosco e estamos com uma ótima oportunIdade!! 🤩🤩\n    
 Você como nosso aluno de tecnologia pode usar as disciplinas que cursou e aproveitar em outra graduação de tecnologia dependendo da situação você pode se formar em outra graduação em 6 meses!!!!!!!!`
 },
 {
-    id: "Teste de Carreira",
-    name: "Teste de Carreira",
-    texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac. 🤓\n
+    Id: "Teste de Carreira",
+    Titulo: "Teste de Carreira",
+    Texto: `Boa tarde, tudo bem? Me chamo Natalí e sou consultora de vendas da Faculdade Senac. 🤓\n
 Verificamos que fizeste o teste de carreira na plataforma Workalove.
 Atráves das opções que recebeu adequado com seu perfil, teria alguma graduação do seu interesse?\n
 Poderia te ajudar?🤩`
 },
 {
-    "id": "Prospec\u00E7\u00E3o",
-    "name": "Prospec\u00E7\u00E3o",
-    "texto": "Boa tarde, tudo bem? Me chamo Natal\u00ED e sou consultora de vendas da Faculdade Senac. \uD83E\uDD29\n\nEstou entrando em contato com voc\u00EA para te perguntar, voc\u00EA j\u00E1 \u00E9 graduado?\n\nSe n\u00E3o!!! O que falta? \uD83D\uDE31\n\nBora se desenvolver? \uD83D\uDCAA\n\nEstamos com bolsas e ofertas para voc\u00EA se desenvolver e crescer no mercado de trabalho.\nTens interesse em saber maiores informa\u00E7\u00F5es? \u263A\uFE0F"
+    "Id": "Prospec\u00E7\u00E3o",
+    "Titulo": "Prospec\u00E7\u00E3o",
+    "Texto": "Boa tarde, tudo bem? Me chamo Natal\u00ED e sou consultora de vendas da Faculdade Senac. \uD83E\uDD29\n\nEstou entrando em contato com voc\u00EA para te perguntar, voc\u00EA j\u00E1 \u00E9 graduado?\n\nSe n\u00E3o!!! O que falta? \uD83D\uDE31\n\nBora se desenvolver? \uD83D\uDCAA\n\nEstamos com bolsas e ofertas para voc\u00EA se desenvolver e crescer no mercado de trabalho.\nTens interesse em saber maiores informa\u00E7\u00F5es? \u263A\uFE0F"
   }
     ]
 }
